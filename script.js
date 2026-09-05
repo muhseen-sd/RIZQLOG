ad619ae742a856f4af7502ab9964b35a3a647ef0
MuhseenSD <183895651+muhseen-sd@users.noreply.github.com>
2026-09-05 11:40:16 +0100

    Back to building RIZQLOG after a long break to ace my school examination.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const transactions = []
 
+const list = document.getElementById("transaction-list")
 const form = document.getElementById("transaction-form")
 
 form.addEventListener("submit", (event) => {
@@ -7,7 +8,7 @@ form.addEventListener("submit", (event) => {
 
     const amontValue = document.getElementById("amount-input").value;
     const categoryValue = document.getElementById("category-select").value;
-    const typeValue = document.querySelector('input[name=type]:checked').value;
+    const typeValue = document.querySelector('input[name=type]:checked')?.value;
     const dateValue = document.getElementById("date").value;
 
     const transactionObj = {
@@ -18,8 +19,24 @@ form.addEventListener("submit", (event) => {
         date: dateValue,
     }
 
-    transactions.push(transactionObj)
+    transactions.push(transactionObj);
+    renderTransactions();
+    form.reset()
 
     console.log("Submitted Transaction:", transactionObj );
     console.log("All Transactions:", transactions)
 })
+
+function renderTransactions(){
+    list.innerHTML = "";
+
+    transactions.forEach(trans => {
+        const li = document.createElement("li")
+        li.textContent = `${trans.date} | ${trans.category} | ${trans.type} | $${trans.amount}`
+
+        list.appendChild(li)
+    })
+
+
+}
+

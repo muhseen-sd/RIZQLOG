132618c367de184422043ca2ecd93e2b4d66bb76
MuhseenSD <183895651+muhseen-sd@users.noreply.github.com>
2026-09-15 16:56:31 +0100

    added a delete button to the list in order to be able to delete a list

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ const form = document.getElementById("transaction-form")
 form.addEventListener("submit", (event) => {
     event.preventDefault()
 
-    const amontValue = document.getElementById("amount-input").value;
+    const amountValue = document.getElementById("amount-input").value;
     const categoryValue = document.getElementById("category-select").value;
     const typeValue = document.querySelector('input[name=type]:checked')?.value;
     const dateValue = document.getElementById("date").value;
 
     const transactionObj = {
         id: Date.now(),
-        amount: Number(amontValue),
+        amount: Number(amountValue),
         category: categoryValue,
         type: typeValue,
         date: dateValue,
@@ -33,10 +33,30 @@ function renderTransactions(){
     transactions.forEach(trans => {
         const li = document.createElement("li")
         li.textContent = `${trans.date} | ${trans.category} | ${trans.type} | $${trans.amount}`
+        
+        // creating a delete button
+        const deleteBtn = document.createElement("button")
+        deleteBtn.textContent = "Delete"
 
+        deleteBtn.addEventListener("click", () => {
+            console.log("Delete id", trans.id)
+
+            deleteTransaction(trans.id)
+        })
+        
+        li.appendChild(deleteBtn)
         list.appendChild(li)
     })
 
-
 }
 
+function deleteTransaction(id){
+            const updatedTransactions = transactions.filter(trans => {
+               return trans.id !== id
+            })
+
+            transactions.length = 0;
+            transactions.push(...updatedTransactions)
+
+            renderTransactions()
+    }
